3a4b2d79b5320b3f0326f1a1c6ea5769dd8ff09b
18819052272@163.com <18819052272@163.com>
2022-08-11 09:30:34 +0800

    feat:    1.还差扫描图库的功能

--- app/src/main/java/com/kandao/smartqrcode/ui/fragment/ScanFragment.kt
+++ app/src/main/java/com/kandao/smartqrcode/ui/fragment/ScanFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 import com.google.zxing.Result
 import com.kandao.smartqrcode.R
-import com.kandao.smartqrcode.utils.StatusBarUtils
 import com.king.zxing.CaptureFragment
 import com.king.zxing.DecodeConfig
 import com.king.zxing.DecodeFormatManager
 import com.king.zxing.analyze.MultiFormatAnalyzer
 import com.king.zxing.config.ResolutionCameraConfig
 import com.theswitchbot.common.logger.Logger
+import java.util.Timer
+import java.util.TimerTask
 
 /**
  * <pre>
@@ -23,10 +23,18 @@ import com.theswitchbot.common.logger.Logger
  */
 class ScanFragment : CaptureFragment() {
 
+  private var timer: Timer?=null
+  private var state:Int = WARN2
+  private val intervalTime:Long = 5000
+
   companion object {
     @JvmStatic
     fun newInstance() = ScanFragment()
+    const val WARN1=1
+    const val WARN2=2
+    const val WARN3=3
   }
+
 
   override fun getLayoutId(): Int {
     return R.layout.fragment_scan
@@ -34,11 +42,52 @@ class ScanFragment : CaptureFragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    val toolbar: Toolbar = rootView.findViewById<Toolbar>(R.id.toolbar)
-    StatusBarUtils.immersiveStatusBar(requireActivity(), toolbar, 0.2f)
-    val tvTitle = rootView.findViewById<TextView>(R.id.tvTitle)
-    tvTitle.text = getString(R.string.main_menu_title_scan)
-    rootView.findViewById<ImageView>(R.id.ivLeft).setOnClickListener { requireActivity().finish() }
+    timer = Timer()
+    setListener()
+    circleShowTips()
+  }
+
+  private fun setListener(){
+    rootView.findViewById<ImageView>(R.id.btnLight).setOnClickListener {
+      toggleLightState()
+    }
+    rootView.findViewById<ImageView>(R.id.btnPhoto).setOnClickListener {
+
+    }
+  }
+
+  private fun circleShowTips(){
+    val tips = listOf(getString(R.string.qr_code_tips1),getString(R.string.qr_code_tips2),getString(R.string.qr_code_tips3))
+    timer?.schedule(object :TimerTask(){
+      override fun run() {
+        requireActivity().runOnUiThread {
+          rootView.findViewById<TextView>(R.id.tvCodeTip).text = tips[state-1]
+        }
+        when (state) {
+          WARN1 -> {
+            state = WARN2
+          }
+          WARN2 -> {
+            state = WARN3
+          }
+          WARN3 -> {
+            state = WARN1
+          }
+        }
+      }
+    },intervalTime,intervalTime)
+  }
+
+
+  /**
+   * 切换闪光灯状态（开启/关闭）
+   */
+  private fun toggleLightState() {
+    if (cameraScan != null) {
+      val isTorch = cameraScan.isTorchEnabled
+      cameraScan.enableTorch(!isTorch)
+      rootView.findViewById<ImageView>(R.id.btnLight).isSelected = !isTorch
+    }
   }
 
   override fun initCameraScan() {
@@ -58,6 +107,11 @@ class ScanFragment : CaptureFragment() {
       .setOnScanResultCallback(this)
       .setAnalyzer(MultiFormatAnalyzer(decodeConfig))
       .setAnalyzeImage(true)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    timer?.cancel()
   }
 
   override fun onScanResultCallback(result: Result): Boolean {
